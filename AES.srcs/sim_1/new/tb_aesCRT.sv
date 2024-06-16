7b989a2bfbc992471b23d82541e9ce2831434249
HM-Huong <hmhuong1408@outlook.com>
2024-06-16 15:27:53 +0700

    test: update tb_aesCRT

--- AES.srcs/sim_1/new/tb_aesCRT.sv
+++ AES.srcs/sim_1/new/tb_aesCRT.sv
@@ -18,14 +18,19 @@ module tb_aesCRT ();
 		.oBlock(oBlock),
 		.idle(done)
 	);
-	logic ok;
+	logic ok         ;
+	int   testNum = 0;
+	int   error   = 0;
+
 	task automatic test(
 			input logic[127:0] in,
 			input logic[127:0] expected
 		);
+		testNum = testNum + 1;
 		iBlock = in;
+		@(negedge clk);
 		start = 1;
-		repeat(2) @(negedge clk);
+		@(negedge clk);
 		start = 0;
 		ok = 1'b0;
 		@(posedge done);
@@ -39,11 +44,26 @@ module tb_aesCRT ();
 		else
 			begin
 				ok = 1'bx;
+				error = error + 1;
 				$error("[FAILED]");
 				$display("\tiBlock \t= %h", in);
 				$display("\toBlock \t= %h", oBlock);
 				$display("\texpected \t= %h", expected);
 			end
+	endtask
+
+	task automatic loadKeyAndIv(
+			input logic[127:0] keyVal,
+			input logic[127:0] ivVal
+		);
+		key = keyVal;
+		iv = ivVal;
+		@(negedge clk);
+		load = 1;
+		@(negedge clk);
+		load = 0;
+		// wait for key expansion done
+		@(posedge done);
 	endtask
 
 	initial
@@ -55,37 +75,23 @@ module tb_aesCRT ();
 			repeat(2) @(negedge clk);
 			rst = 0;
 
-			$display("===== encrypt iBlock in CTR mode ======");
-			// load key and iv
-			load = 1;
-			key = 128'h70617373776f726450617373776f7264;
-			iv = 128'h30313233343536373839616263646566;
-			repeat(2) @(negedge clk);
-			load = 0;
-			// wait for key expansion done
-			@(posedge done);
-
+			$display("\n\n===== encrypt iBlock in CTR mode ======");
+			loadKeyAndIv(128'h70617373776f726450617373776f7264, 128'h30313233343536373839616263646566);
 			test(128'h486f616e67204d696e682048756f6e67, 128'h09353ccc3dff54567bdedb49ddc8deb9);
 			test(128'h37383935383739353436383938373835, 128'h54427e87a044f594f110c07e53f5a0c8);
 			test(128'h497320746869732061206b65793f3f3f, 128'h704644f8d5824086dac4fb49b80d7ec7);
 			test(128'h5965732c2069742069732061206b6579, 128'h4b53114ccbde893540e8dca8581259d2);
 			test(128'h31343532363937353235393633343837, 128'h01d1eef69644f6478fe7aed1104561b3);
 
-			$display("===== decrypt oBlock in CTR mode ======");
-			// load key and iv
-			load = 1;
-			key = 128'h70617373776f726450617373776f7264;
-			iv = 128'h30313233343536373839616263646566;
-			repeat(2) @(negedge clk);
-			load = 0;
-			// wait for key expansion done
-			@(posedge done);
-
+			$display("\n\n===== decrypt oBlock in CTR mode ======");
+			loadKeyAndIv(128'h70617373776f726450617373776f7264, 128'h30313233343536373839616263646566);
 			test(128'h09353ccc3dff54567bdedb49ddc8deb9, 128'h486f616e67204d696e682048756f6e67);
 			test(128'h54427e87a044f594f110c07e53f5a0c8, 128'h37383935383739353436383938373835);
 			test(128'h704644f8d5824086dac4fb49b80d7ec7, 128'h497320746869732061206b65793f3f3f);
 			test(128'h4b53114ccbde893540e8dca8581259d2, 128'h5965732c2069742069732061206b6579);
 			test(128'h01d1eef69644f6478fe7aed1104561b3, 128'h31343532363937353235393633343837);
+
+			$display("\n\n%d tests completed with %4d errors\n\n", testNum, error);
 
 			// stop simulation
 			$finish;
